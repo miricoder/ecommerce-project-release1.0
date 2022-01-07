13c2c345800f0bb536cccc321b1440ffd35d85cb
Mirali Mirzayev <miralimirzayev@Miralis-MacBook-Pro.local>
2022-01-07 18:41:13 -0500

    Updating angular.json and finishing Environment configuration for the App

--- 03-frontend/angular-ecommerce/src/app/services/product.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/product.service.ts
@@ -6,6 +6,7 @@ import { map } from 'rxjs/operators';
 import { ProductCategory } from '../common/product-category';
 import { environment } from 'src/environments/environment';
 
+
 @Injectable({
   providedIn: 'root'
 })
